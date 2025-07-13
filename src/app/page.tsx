f44f2ec038e39d16fc5d5015c6716df75779c878
Arthur Horyu <1horyuarthur@gmail.com>
2025-07-13 06:11:14 +0000

    a imagem grandona do legado da ponte de pedar tá quebrado, pode ter ocor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const techProjects: Project[] = [
     {
         title: "Legado da Ponte de Pedra (Site and Short-film)",
         description: "Este é o repositório oficial do projeto \"Legado da Ponte de Pedra\". Mais do que um site, é uma plataforma de conscientização e mobilização para a preservação de um dos mais importantes patrimônios geológicos, arqueológicos e culturais de Goiás, localizado na divisa entre Rio Verde e Paraúna.",
-        image: "https://camo.githubusercontent.com/46bbefc1f593e73f2fc424f03875642b2d6ef5ec4cdcfeebdf328b52b15801ba/68747470733a2f2f692e706f7374696d672e63632f78546758784377502f33616432346535663661633466313838373764303836383832653830632e6a7067",
+        image: "https://i.postimg.cc/xTgXxCWp/3ad24e5f6ac4f18877d086882e80c.jpg",
         hint: "bridge stone",
         link: "https://ponte-de-pedra.vercel.app/",
         specialLink: "https://www.change.org/p/salve-a-ponte-de-pedra-pch-prev%C3%AA-impacto-negativo-em-patrim%C3%B4nio-ambiental-e-arquiol%C3%B3gico?source_location=psf_petitions",
