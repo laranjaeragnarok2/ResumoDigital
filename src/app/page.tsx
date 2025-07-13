eb8f490fb19f539a9b062507d0a7d50936a09a87
Arthur Horyu <1horyuarthur@gmail.com>
2025-07-13 06:15:21 +0000

    Legado da ponte de pedra : https://i.postimg.cc/gJrxYgyq/68747470733a2f2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const techProjects: Project[] = [
     {
         title: "Legado da Ponte de Pedra (Site and Short-film)",
         description: "Este é o repositório oficial do projeto \"Legado da Ponte de Pedra\". Mais do que um site, é uma plataforma de conscientização e mobilização para a preservação de um dos mais importantes patrimônios geológicos, arqueológicos e culturais de Goiás, localizado na divisa entre Rio Verde e Paraúna.",
-        image: "https://camo.githubusercontent.com/46bbefc1f593e73f2fc424f03875642b2d6ef5ec4cdcfeebdf328b52b15801ba/68747470733a2f2f692e706f7374696d672e63632f78546758784377502f33646132346535663661633466313838373764303836383832653830632e6a7067",
+        image: "https://i.postimg.cc/gJrxYgyq/68747470733a2f2f692e706f7374696d672e63632f78546758784377502f3364613234653566366163346631383837376430.jpg",
         hint: "bridge stone",
         link: "https://ponte-de-pedra.vercel.app/",
         specialLink: "https://www.change.org/p/salve-a-ponte-de-pedra-pch-prev%C3%AA-impacto-negativo-em-patrim%C3%B4nio-ambiental-e-arquiol%C3%B3gico?source_location=psf_petitions",
@@ -18,7 +18,7 @@ const techProjects: Project[] = [
     {
         title: "Medusa Store (E-commerce Headless)",
         description: "Landing page para e-commerce com lista de espera via Formspree, notificações por e-mail, painel de controle, design responsivo com tema escuro e botões flutuantes para contato direto.",
-        image: "https://placehold.co/600x400.png",
+        image: "https://i.postimg.cc/PqSLsgD5/68747470733a2f2f692e706f7374696d672e63632f4a373644684a475a2f73637265656e636170747572652d6d6564757361.png",
         hint: "medusa store",
         link: "https://github.com/laranjaeragnarok2/medusa-store",
         tags: ["Medusa.js", "Next.js", "PostgreSQL"],
